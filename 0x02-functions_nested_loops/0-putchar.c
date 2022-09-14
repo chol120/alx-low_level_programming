faf1e335320cc14f1e48c29697061a5cc2a99632
chol120 <cholmanyang120@gmai.com>
2022-09-14 13:58:47 -0700

    Commit for task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,15 +3,6 @@
 
 
 /**
-<<<<<<< HEAD
-
- * main - print putchar
-
- * Return: always 0
-
- */
-
-=======
  *
  *  * main - print putchar
  *
@@ -19,39 +10,12 @@
  *
  *    */
 
->>>>>>> 9c06a5f8e0ec9d576768b949c9a995a3986e5c4e
 
 
 int main(void)
 
 {
 
-<<<<<<< HEAD
-	_putchar('_');
-
-	_putchar('p');
-
-	_putchar('u');
-
-	_putchar('t');
-
-	_putchar('c');
-
-	_putchar('h');
-
-	_putchar('a');
-
-	_putchar('r');
-
-	_putchar('\n');
-
-
-
-	return (0);
-
-
-
-=======
 		_putchar('_');
 
 			_putchar('p');
@@ -76,5 +40,4 @@ int main(void)
 
 
 
-}i
->>>>>>> 9c06a5f8e0ec9d576768b949c9a995a3986e5c4e
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,20 +3,6 @@
 
 
 /**
-<<<<<<< HEAD
-
- * _putchar - writes the character c to stdout
-
- * @c: The character to print
-
- *
-
- * Return: On success 1.
-
- * On error, -1 is returned, and errno is set appropriately.
-
- */
-=======
  *
  *  * _putchar - writes the character c to stdout
  *
@@ -29,17 +15,11 @@
  *      * On error, -1 is returned, and errno is set appropriately.
  *
  *       */
->>>>>>> 9c06a5f8e0ec9d576768b949c9a995a3986e5c4e
 
 int _putchar(char c)
 
 {
 
-<<<<<<< HEAD
-  return (write(1, &c, 1));
-
-=======
 	  return (write(1, &c, 1));
 
 }
->>>>>>> 9c06a5f8e0ec9d576768b949c9a995a3986e5c4e
