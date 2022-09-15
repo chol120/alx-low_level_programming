3b4d7a03619cb2b18db574fa496bb52e3fac2349
chol120 <cholmanyang120@gmai.com>
2022-09-15 07:08:50 -0700

    Commit for putchar task zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,39 +4,29 @@
 
 /**
 
- * main - print putchar
+*_isupper - checks if a  character is uppercase or not
 
- * Return: always 0
+*@c: character to be tested
 
- */
+*Return: 1 whether it is, 0 otherwise
+
+*/
 
 
 
-int main(void)
+int _isupper(int c)
 
 {
 
-	_putchar('_');
+	if ((c >= 'A') && (c <= 'Z'))
 
-	_putchar('p');
+	{
 
-	_putchar('u');
+		return (1);
 
-	_putchar('t');
-
-	_putchar('c');
-
-	_putchar('h');
-
-	_putchar('a');
-
-	_putchar('r');
-
-	_putchar('\n');
+	}
 
 
 
 	return (0);
-
-
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,36 +1,44 @@
-#ifndef HOLBERTON_H_INCLUDED
+#ifndef MAIN_H
 
-#define HOLBERTON_H_INCLUDED
+#define MAIN_H
 
 
+
+/**
+
+*main - header file for protoypes
+
+*void protoypes (void)
+
+*int protoypes (int)
+
+*/
+
+
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+void print_number(int n);
 
 int _putchar(char);
 
-void print_alphabet(void);
+int _isupper(int);
 
-void print_alphabet_x10(void);
+int _isdigit(int);
 
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
-
-void num(int r, int n);
+int mul(int, int);
 
 
 
